1432e4dd1baeafffa9748612359c98c920eddbf8
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-05 13:20:28 +0100

    Exercice 2
    - Déclaration de variable type string/float/int/boolean
    - Affiche via echo
    - Utilisation commande var_dump()

---
+++ exercices/jour-01/produit.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Raquette";
+$price = 199.99;
+$price2 = "199.99";
+$stock = 7;
+$onSale = false;
+
+var_dump($name);
+var_dump($price);
+var_dump($stock);
+var_dump($onSale);
+var_dump($price2);
+
+$phrase = "Le Produit X coûte Y€";
+
+echo $phrase;
+
+$difference = "Price avec guillement sera un string donc il s'affichera comme un string";

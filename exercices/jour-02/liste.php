d92103f2f89d4067bdc94e02b9cc96655d4a8be3
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-06 10:16:09 +0100

    Exercice 1
    -Création d'un tableau
    -Manipulation d'un tableau
    -Affichage d'un tableau

---
+++ exercices/jour-02/liste.php
@@ -0,0 +1,16 @@
+<?php
+$groceries = ["chips", "poulet", "comté", "jambon blanc", "oeufs"];
+var_dump($groceries);
+echo "<br>$groceries[0]";
+
+$index = count($groceries);
+echo "<br>Nombre d'article = $index";
+echo '<br> Le/les dernier(s) article(s) est/sont : ' . $groceries[$index - 1];
+
+echo '<br>';
+array_push($groceries, "pain", "yaourt");
+var_dump($groceries);
+
+echo '<br>';
+unset($groceries[1]);
+var_dump($groceries);

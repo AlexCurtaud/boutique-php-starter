4667a9eb29730ed8371932afca3ca56d403a71b4
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-07 10:37:57 +0100

    Exercice 3
    - Utiliser une boucle foreach dans un tableau mutlidimensionnel
    - Utiliser la boucle pour créer des fiches produits en html

---
+++ exercices/jour-03/liste-produits.php
@@ -0,0 +1,36 @@
+<?php
+$product = [
+    [
+        "name" => "Ski 1000",
+        "price" => 349.99,
+        "stock" => 12,
+    ],
+    [
+        "name" => "Ski 1500",
+        "price" => 449.99,
+        "stock" => 16,
+    ],
+    [
+        "name" => "Ski 2000",
+        "price" => 599.99,
+        "stock" => 5,
+    ],
+    [
+        "name" => "Snowboard 1000",
+        "price" => 399.99,
+        "stock" => 19,
+    ],
+    [
+        "name" => "Snowboard 2000",
+        "price" => 549.99,
+        "stock" => 9,
+    ]
+];
+
+foreach ($product as $product) {
+    echo '<article>' .
+        '<h3>' . $product["name"] . '</h3>' .
+        '<p>' . $product["price"] . '€</p>' .
+        '<p>' . $product["stock"] . '</p>' .
+        '</article>';
+}

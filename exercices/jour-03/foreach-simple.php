ddc8e75acbeb0793173cde39b24d38b07b479cdb
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-07 09:36:48 +0100

    Exercice 1
    - Parcourir un tableau simple avec une boucle foreach
    - Générer du code html grâce à cette boucle

---
+++ exercices/jour-03/foreach-simple.php
@@ -0,0 +1,24 @@
+<?php
+$firstNames = ["Johnny", "Bobby", "Randy", "Sandy", "Sanders"];
+$i = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prénoms</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($firstNames as $firstNames) {
+            echo '<li>' . $i . ' ' . $firstNames . '</li>' . '<br>';
+            $i++;
+        } ?>
+    </ul>
+</body>
+
+</html>

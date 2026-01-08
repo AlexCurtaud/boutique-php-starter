64bee9564da5ef50e53da9be980cfc4a0671a82e
AlexCurtaud <alex.curtaud@gmail.com>
2026-01-08 14:23:02 +0100

    Jour 4 - Exercice 6
    - Afficher des produits en fonction d'un filtre
    - Afficher un compteur

---
+++ exercices/jour-04/filter.php
@@ -0,0 +1,77 @@
+<?php
+$product = [
+    [
+        "name" => "Infuse Snowsurf",
+        "price" => 460.00,
+        "stock" => 5,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Invado OG",
+        "price" => 265.00,
+        "stock" => 0,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Hi-Standard OG",
+        "price" => 250.00,
+        "stock" => 7,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Encore OG Women",
+        "price" => 265.00,
+        "stock" => 3,
+        "category" => "Women Snowboard Boots"
+    ],
+    [
+        "name" => "Hi-Standard OG Women",
+        "price" => 250.00,
+        "stock" => 0,
+        "category" => "Women Snowboard Boots"
+    ],
+    [
+        "name" => "Luna Pro",
+        "price" => 355.00,
+        "stock" => 11,
+        "category" => "Women Snowboard Boots"
+    ],
+    [
+        "name" => "Aura Pro",
+        "price" => 375.00,
+        "stock" => 15,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Invado Pro",
+        "price" => 350.00,
+        "stock" => 15,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Hi-Standard Pro",
+        "price" => 350.00,
+        "stock" => 6,
+        "category" => "Men Snowboard Boot"
+    ],
+    [
+        "name" => "Invado Pro Women",
+        "price" => 350.00,
+        "stock" => 6,
+        "category" => "Women Snowboard Boots"
+    ]
+];
+
+$productDisplayed;
+
+for ($i = 0; $i < 11; $i++) {
+    if ($product[$i]["stock"] > 0 && $product[$i]["price"] <= 300) {
+        echo '<div>' .
+            '<h3>' . $product[$i]["name"] . '</h3>' .
+            '<p>' . $product[$i]["price"] . '€</p>' .
+            '</div>';
+        $productDisplayed++;
+    }
+}
+
+echo $productDisplayed . ' products out of ' . count($product);

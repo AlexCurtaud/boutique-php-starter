e6e1ea2cd3bccf9d05488509a3f73e542b266e38
AlexCurtaud <alex.curtaud@gmail.com>
2026-01-08 10:01:21 +0100

    Jour 4 - Exercice 3
    - Apprendre à utiliser Switch & Match
    - Comprendre leurs intérêts et avantages

---
+++ exercices/jour-04/order.php
@@ -0,0 +1,28 @@
+<?php
+$word = "delivered";
+
+switch ($word) {
+case "standby":
+    echo '<p style="color: grey">' . $word . '</p>';
+break;
+case "validated":
+    echo '<p style="color: yellow">' . $word . '</p>';
+break;
+case "shipped":
+    echo '<p style="color: blue">' . $word . '</p>';
+break;
+case "delivered":
+    echo '<p style="color: green">' . $word . '</p>';
+break;
+case "canceled":
+    echo '<p style="color: red">' . $word . '</p>';
+break;
+}
+
+$status = match($word){
+    "standby" => '<p style="color: grey">' . $word . '</p>',
+    "validated" => '<p style="color: yellow">' . $word . '</p>',
+    "shipped" => '<p style="color: blue">' . $word . '</p>',
+    "delivered" => '<p style="color: green">' . $word . '</p>',
+    "canceled" => '<p style="color: red">' . $word . '</p>',
+};

23f936cd3bd4c5b3fc66021840823269e14f98ec
AlexCurtaud <alex.curtaud@gmail.com>
2026-01-09 14:44:40 +0100

    Jour 5 - Exercice 6
    - Création d'un fichier helpers.php contenant
    les fonctions utiles à un projet
    - Association du fichier helpers.php à un autre
    fichier grâce à require_once
    - Permet d'alléger le fichier d'affichage

---
+++ exercices/jour-05/helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+function displayBadge($text, $color)
+{
+    return '<span class="badge" style="background: ' . $color . '">' . '$text' . '</span>';
+}
+
+function displayPrice($price, $discount = 0)
+{
+    if ($discount === 0) {
+        return $price . ' €';
+    } else {
+        return '<span><strike>' . $price . ' €</strike></span>';
+    }
+}
+
+function displayStock($quantity)
+{
+    if ($quantity === 0) {
+        return '<span style="color: red">Rutpure</span>';
+    } else if ($quantity > 0 && $quantity < 4) {
+        return '<span style="color: orange">Dernières unités disponibles</span>';
+    } else {
+        return '<span style="color: green">En stock</span>';
+    }
+}
---
+++ exercices/jour-05/page.php
@@ -0,0 +1,21 @@
+<?php
+require_once "helpers.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?= displayBadge("Boost", "green") ?>
+    <?= displayPrice(150, $discount = 0.2) ?>
+    <?= displayStock(0) ?>
+</body>
+
+</html>

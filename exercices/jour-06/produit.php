ae8de7798a22ca21fed0de0da3957ee8f78f88d7
AlexCurtaud <alex.curtaud@gmail.com>
2026-01-12 14:26:05 +0100

    Jour 06 - Exercice 2
    - Utilisation de l'id d'un tableau via l'url
    - Determiné l'affichage de la page en fonction de
    la donnée de cet id

---
+++ exercices/jour-06/produit.php
@@ -0,0 +1,20 @@
+<?php
+$product = [
+    1 => ["name" => "boost1", "price" => 49.99],
+    2 => ["name" => "boost2", "price" => 79.99],
+    3 => ["name" => "boost3", "price" => 119.99],
+    4 => ["name" => "boost4", "price" => 169.99],
+    5 => ["name" => "boost5", "price" => 199.99]
+];
+
+$id = $_GET["id"] ?? null;
+
+function productCard($product, $id)
+{
+    if (array_key_exists($id, $product)) {
+        return $product[$id]["name"] . ' ' . $product[$id]["price"];
+    }
+    return "Please try again with validate datas";
+}
+
+echo productCard($product, $id);

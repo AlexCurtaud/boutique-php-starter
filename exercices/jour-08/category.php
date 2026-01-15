dbd3dc3bd0bf6fbbd22b16afc9f1b8f3b1dee575
AlexCurtaud <alex.curtaud@gmail.com>
2026-01-15 22:40:49 +0100

    Jour 08 Exercice 6
    - Création d'une fonction qui transforme le nom en
    minuscule et qui remplace les expaces par des
    tirets pour la description

---
+++ exercices/jour-08/category.php
@@ -0,0 +1,26 @@
+<?php
+
+class Category
+{
+    public function __construct(
+        public int $id,
+        public string $nom,
+        public string $description
+    ) {}
+
+    public function getSlug()
+    {
+        echo strtolower($this->nom);
+        echo str_replace(" ", "-", $this->description);
+    }
+}
+
+$cat1 = new Category(1, "Small", "A tiny item, useful everywhere");
+$cat2 = new Category(2, "Medium", "A standard size item, average");
+$cat3 = new Category(3, "Big", "A huge item, we don't know where you willl store it!");
+
+$cat1->getSlug();
+echo '<br>';
+$cat2->getSlug();
+echo '<br>';
+$cat3->getSlug();

d787694efd1b9ce3e3a2e038507beddc4b7e58ff
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-15 14:42:59 +0100

    Jour 8 Exercice 2
    - Apprendre le constructeur de classe

---
+++ exercices/jour-08/User.php
@@ -0,0 +1,27 @@
+<?php
+
+class User
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public string $registrationDate
+    ) {
+        if ($registrationDate === NULL) {
+            $registrationDate = date("Y-m-d");
+        }
+    }
+
+    public function isNewMember()
+    {
+        $diff = (strtotime(date("Y-m-d")) - strtotime($this->registrationDate)) / (60 * 60 * 24);
+        var_dump($diff);
+        if ($diff < 30) {
+            return "true";
+        }
+        return "false";
+    }
+}
+
+$user1 = new User("Alex", "alex@gmail.com", "2024-12-01");
+echo $user1->isNewMember();

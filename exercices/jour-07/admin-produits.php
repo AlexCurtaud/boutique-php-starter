60052eac56efb444ee8174ec4fc3f81c35996f8d
Alex Curtaud <alex.curtaud@gmail.com>
2026-01-16 16:56:31 +0100

    Jour 07 Exercice 6 Update

--- exercices/jour-07/admin-produits.php
+++ exercices/jour-07/admin-produits.php
@@ -1,11 +1,16 @@
 <?php
 
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=boutique;charset=utf8mb4",
-    "dev",
-    "dev",
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=boutique;charset=utf8mb4",
+        "dev",
+        "dev",
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+    echo "✅ Connexion réussie !";
+} catch (PDOException $e) {
+    echo "❌ Erreur : " . $e->getMessage();
+}
 
 $productName = $_POST["product-name"];
 $price = $_POST["price"];
